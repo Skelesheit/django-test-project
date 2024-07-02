0163b3fe9ce1aec24f73ae342a4d51edcd39a2c3
Skelesheit <Skelesheit@gmail.com>
2024-07-02 12:31:13 +0300

    has written first views ChatApiView

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from chat import models
+
 # Register your models here.
+
+admin.site.register(models.Chat)
+
+admin.site.register(models.CustomUser)
+
+admin.site.register(models.ChatParticipant)
+
+admin.site.register(models.Message)
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from rest_framework.views import APIView
+
+from rest_framework.response import Response
+
+from chat.models import Chat
+
 
 # Create your views here.
+
+
+class ChatApiView(APIView):
+    def get(self, request):
+        lst = Chat.objects.all()
+        return Response({"message": "Hello from get method!", "data": list(lst)})
+
+    def post(self, request):
+        return Response({"message": "Hello from post method!", "data": Chat.objects.all()})
